212b84eedc325d6abf827535e0d4a6f8fc804b89
Hiram <hiram.artnak@gmail.com>
2023-12-02 20:50:38 -0300

    Add comments to mark custom folds

--- llist.c
+++ llist.c
@@ -15,13 +15,13 @@ llist_node *llist_node_create(void *data);
 void llist_node_free(llist_node *node, llist_free_fn free_fn);
 
 llist_node *llist_node_create(void *data) {
-
+    // Allocate Memory //
     llist_node *node = malloc(sizeof(llist_node));
     if(node == NULL) {
         fprintf(stderr, "Error: malloc failed allocating node\n");
         exit(1);
     }
-
+    // Set node attributes //
     node->data = data;
     node->next = NULL;
     node->prev = NULL;
@@ -30,6 +30,7 @@ llist_node *llist_node_create(void *data) {
 
 
 void llist_node_free(llist_node *node, llist_free_fn free_fn) {
+    // Validate inputs //
     if(node == NULL) {
         fprintf(stderr, "Error: tried to free node, but node is NULL\n");
         exit(1);
@@ -38,7 +39,6 @@ void llist_node_free(llist_node *node, llist_free_fn free_fn) {
         fprintf(stderr, "Error: tried to free data from node, but node->data is NULL\n");
         exit(1);
     }
-
     // Use the free function if it exists, otherwise use free
     if (free_fn != NULL) {
         free_fn(node->data);
@@ -59,11 +59,13 @@ typedef struct llist {
 
 // Allocate a new linked list
 llist *llist_create(llist_free_fn free_fn) {
+    // Allocate Memory //
     llist *list = malloc(sizeof(llist));
     if (list == NULL) {
         fprintf(stderr, "Error: malloc failed allocating list\n");
         exit(1);
     }
+    // Set list attributes //
     list->head = NULL;
     list->tail = NULL;
     list->free_fn = free_fn;
@@ -73,47 +75,59 @@ llist *llist_create(llist_free_fn free_fn) {
 
 // Free a linked list
 void llist_free(llist *list){
+    // Validate inputs //
     if (list == NULL) {
         fprintf(stderr, "Error: tried to free list, but list is NULL\n");
         exit(1);
     }
+    // Loop through the list and free each node //
     llist_node *node = list->head;
     while (node != NULL) {
         llist_node *next = node->next;
         llist_node_free(node, list->free_fn);
         node = next;
+        list->length--;
     }
+    // Check invariant //
+    if(list->length != 0) {
+        fprintf(stderr, "Error: list length is not zero after freeing all nodes\n");
+        exit(1);
+    }
+    // Set the head and tail to NULL//
     list->head = NULL;
     list->tail = NULL;
-    list->length = 0;
     free(list);
 }
 
 
 // Append a node to the end of the list
 void llist_append(llist *list, void *data){
+    // Validate inputs //
     if(list == NULL) {
         fprintf(stderr, "Error: tried to append to list, but list is NULL\n");
         exit(1);
     }
+    
     llist_node *node = llist_node_create(data);
+    
     // If the list is empty then set the head and tail to the new node
     if (list->head == NULL) {
         list->head = node;
         list->tail = node;
-    } else {
-        // Otherwise, set the tail's next to the new node
+    } else { // Otherwise, set the tail's next to the new node
         list->tail->next = node;
         // Set the new node's prev to the tail
         node->prev = list->tail;
         // Set the tail to the new node
         list->tail = node;
     }
+    
     list->length++;
 }
 
 // Get the data based on index
 void *llist_get(llist *list, int index){
+    // Validate inputs //
     if(list == NULL) {
         fprintf(stderr, "Error: tried to get from list, but list is NULL\n");
         exit(1);
@@ -122,6 +136,7 @@ void *llist_get(llist *list, int index){
         fprintf(stderr, "Error: tried to get from list, but index is out of bounds\n");
         exit(1);
     }
+    // Loop through the list until we get to the index //
     llist_node *node = list->head;
     for(int i = 0; i < index; i++) {
         node = node->next;
@@ -131,10 +146,12 @@ void *llist_get(llist *list, int index){
 
 // Remove a node from the list
 void llist_remove(llist *list, void *key, llist_compare_fn compare_fn){
+    // Validate inputs //
     if(list == NULL) {
         fprintf(stderr, "Error: tried to remove from list, but list is NULL\n");
         exit(1);
     }
+    // Loop through the list until we find the key //
     llist_node *node = list->head;
     while(node != NULL) {
         if(compare_fn(key, node->data) == 0) {
@@ -143,6 +160,9 @@ void llist_remove(llist *list, void *key, llist_compare_fn compare_fn){
                 list->head = node->next;
                 if(list->head != NULL) {
                     list->head->prev = NULL;
+                }else{
+                    // The list is now empty
+                    list->tail = NULL;
                 }
             } else {
                 // If the node is the tail
@@ -163,16 +183,31 @@ void llist_remove(llist *list, void *key, llist_compare_fn compare_fn){
         }
         node = node->next;
     }
+    // If we get here, the key was not found
+    fprintf(stderr, "Error: tried to remove from list, but data with matching key was not found\n");
+    exit(1);
 }
+
 
 // Print the list
 void llist_print(llist *list, llist_print_fn print_fn){
+    // Validate inputs //
+    
     if(list == NULL) {
         fprintf(stderr, "Error: tried to print list, but list is NULL\n");
         exit(1);
     }
+    if(print_fn == NULL) {
+            fprintf(stderr, "Error: tried to print list, but print function is NULL\n");
+            exit(1);
+    }
+    // Loop and print //
     llist_node *node = list->head;
     while(node != NULL) {
+        if(node->data == NULL) {
+            fprintf(stderr, "Error: tried to print list, but data is NULL\n");
+            exit(1);
+        }
         print_fn(node->data);
         node = node->next;
     }
@@ -180,6 +215,7 @@ void llist_print(llist *list, llist_print_fn print_fn){
 
 // Get the length of the list
 int llist_length(llist *list){
+    // Validate inputs //
     if(list == NULL) {
         fprintf(stderr, "Error: tried to get length of list, but list is NULL\n");
         exit(1);
