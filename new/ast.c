935a0643488632ada89c584c08e71cb7dcf61b56
Hiram Artnak Martins <hiram.artnak@gmail.com>
2024-01-26 01:23:15 -0300

    fix print not working for empty nodes

--- new/ast.c
+++ new/ast.c
@@ -154,10 +154,8 @@ void ast_node_print(ast_node *node){
 }
 
 void ast_node_print_tree(ast_node *node){
-    if(node->type == AST_NODE_TYPE_EMPTY){
-        return;
-    }
-    if(node->type == AST_NODE_TYPE_PROGRAM_START){
+
+    if(node->type == AST_NODE_TYPE_PROGRAM_START || node->type == AST_NODE_TYPE_EMPTY){
         for(int i = 0; i < ast_node_list_size(node->children); i++){
             ast_node *child = ast_node_list_get(node->children, i);
             ast_node_print_tree(child);
